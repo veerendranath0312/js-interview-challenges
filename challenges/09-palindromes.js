ddc3332c211da369751235a0581fb99499fee7dd
Veerendranath <veerendranath0312@gmail.com>
2023-03-11 16:26:46 +0000

    Add solutions for challenges 8, 9, & 10

---
+++ challenges/09-palindromes.js
@@ -0,0 +1,37 @@
+/* 
+Palindromes are words that are the same forward or backward. For example, 
+the words "noon" and "kayak" are a palindromes.
+ 
+Write a function to check if a lowercased string of letters is a palindrome. 
+If the word is palindrome, return true. If it isn't, return false.
+
+Example input: "motorbike"
+Example output: false
+
+Example input: "rotator" 
+Example output: true
+*/
+
+// Using built-in methods
+function isPalindrome(str) {
+  const reverseStr = str.split("").reverse().join("");
+  return str === reverseStr;
+}
+
+// Without using built-in methods
+// function isPalindrome(str) {
+//   let reverseStr = "";
+
+//   for (let i = str.length - 1; i >= 0; i--) {
+//     reverseStr += str[i];
+//   }
+
+//   return str === reverseStr;
+// }
+
+// Test your function
+console.log(isPalindrome("abba"));
+console.log(isPalindrome("civic"));
+console.log(isPalindrome("octopus"));
+console.log(isPalindrome("pumpkins"));
+console.log(isPalindrome("madam"));
---
+++ challenges/10-save-grandpa-password.js
@@ -0,0 +1,30 @@
+/*  
+Grandpa's hand isn't as steady as it used to be. You finally convinced him
+to start using a password manager, but he accidentally typed and saved his
+password with a bunch of extra characters. Help him recover his password by 
+removing all the duplicate characters. 
+
+Your function should take in a string of characters and return a
+string with the duplicate characters removed. Assume that your input
+is lowercased with only letters and numbers.  
+
+Example input: "aabbccb1212"
+Example output: "abc12"
+*/
+const password = "9338dsabbbadjdjdj2sdfdfdf282ff8fdsd888ss8cfgfg332q23";
+
+function removeDupeChars(password) {
+  // We can achieve this using the new ES6 feature: Set()
+
+  // Split the string and make into an array
+  const chars = password.split("");
+
+  // Using Set() get the unique chars set,
+  // spread the Set() to convert into array, and join
+  const uniquePassword = [...new Set(chars)].join("");
+
+  return uniquePassword;
+}
+
+console.log(removeDupeChars(password));
+console.log(removeDupeChars("aabbccb1212"));
